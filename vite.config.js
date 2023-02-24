db040783074a6fcb06a8da3c4c8af01e1df084cc
Vite84 <vite-js-84@tutanota.com>
2023-02-24 07:55:50 +0100

    Publish

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ export default {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                french: resolve(__dirname, 'french.html')
+                french: resolve(__dirname, 'french.html'),
+                buy: resolve(__dirname, 'buyDLM.html'),
+                acheter: resolve(__dirname, 'acheterDLM.html'),
             }
         }
     }
